396226a92f9a5960623142be1203be90a01080d6
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2024-07-17 21:02:42 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <ColorPicker />
+    </div>
+  );
+};
+
+export default App;
